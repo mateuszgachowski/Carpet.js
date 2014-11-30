7ea53b79b955d8d97a9564f17b18f50724360d98
Mateusz Gachowski <mateusz.gachowski@gmail.com>
2014-11-30 01:28:02 +0100

    modify: check new sauce config

--- karma.conf.js
+++ karma.conf.js
@@ -2,40 +2,40 @@ module.exports = function (config) {
 
   var customLaunchers = {
     // Sauce labs
-    // 'SL_Chrome': {
-    //   base: 'SauceLabs',
-    //   browserName: 'chrome',
-    //   version: '34'
-    // },
-    // 'SL_Firefox': {
-    //   base: 'SauceLabs',
-    //   browserName: 'firefox',
-    //   version: '26'
-    // },
-    // 'SL_Safari': {
-    //   base: 'SauceLabs',
-    //   browserName: 'safari',
-    //   platform: 'OS X 10.9',
-    //   version: '7'
-    // },
-    // 'SL_IE_9': {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 2008',
-    //   version: '9'
-    // },
-    // 'SL_IE_10': {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 2012',
-    //   version: '10'
-    // },
-    // 'SL_IE_11': {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 8.1',
-    //   version: '11'
-    // },
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      version: '34'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '26'
+    },
+    'SL_Safari': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    },
+    'SL_IE_9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE_10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE_11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
 
     // BrowserStack
     'BS_Chrome': {
@@ -87,7 +87,11 @@ module.exports = function (config) {
     },
 
     sauceLabs: {
-      testName: 'Carpet.js Unit Tests'
+      testName: 'Carpet.js Unit Tests',
+      startConnect: true,
+      options: {
+        'selenium-version': '2.41.0'
+      }
     },
 
     basePath: '',
