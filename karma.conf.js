a0b92d9cba209e9dc156f1f8344d474b6a65dc2f
Mateusz Gachowski <mateusz.gachowski@gmail.com>
2014-11-30 01:48:44 +0100

    remove: saucelabs from the testing suite

--- karma.conf.js
+++ karma.conf.js
@@ -1,54 +1,19 @@
 module.exports = function (config) {
 
   var customLaunchers = {
-    // Sauce labs
-    'SL_Chrome': {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      version: '34'
-    },
-    'SL_Firefox': {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '26'
-    },
-    'SL_Safari': {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'OS X 10.9',
-      version: '7'
-    },
-    'SL_IE_9': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2008',
-      version: '9'
-    },
-    'SL_IE_10': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 2012',
-      version: '10'
-    },
-    'SL_IE_11': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
-    },
 
     // BrowserStack
     'BS_Chrome': {
       base: 'BrowserStack',
       browser: 'chrome',
       os: 'OS X',
-      os_version: 'Mountain Lion'
+      os_version: 'Yosemite'
     },
     'BS_Safari': {
       base: 'BrowserStack',
       browser: 'safari',
       os: 'OS X',
-      os_version: 'Mountain Lion'
+      os_version: 'Yosemite'
     },
     'BS_Firefox': {
       base: 'BrowserStack',
@@ -86,16 +51,6 @@ module.exports = function (config) {
       accessKey: process.env.BROWSER_STACK_ACCESS_KEY
     },
 
-    sauceLabs: {
-      testName: 'Carpet.js Unit Tests',
-      startConnect: true,
-      options: {
-        'selenium-version': '2.41.0'
-      }
-    },
-
-    basePath: '',
-
     frameworks: ['jasmine'],
 
     files: [
@@ -103,26 +58,13 @@ module.exports = function (config) {
       'tests/spec/carpet.test.js'
     ],
 
-    exclude: [
-
-    ],
-
-    preprocessors: {},
-
-    reporters: ['dots', 'saucelabs'],
-
-    port: 9876,
-
+    reporters: ['dots'],
     colors: true,
-
     logLevel: config.LOG_INFO,
-
     autoWatch: true,
-
     singleRun: true,
 
     customLaunchers: customLaunchers,
-
     browsers: Object.keys(customLaunchers)
   });
 };
