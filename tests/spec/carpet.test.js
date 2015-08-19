42d711ca6d0591a456c3b47cdca175e188178b3a
Przemek Sobstel <przemek@sobstel.org>
2015-08-19 19:14:05 +0200

    test for init(element)

--- tests/spec/carpet.test.js
+++ tests/spec/carpet.test.js
@@ -139,6 +139,29 @@
       window.Carpet.init();
     });
 
+    it('should look for modules inside of specifiec DOM element only if passed to init()', function (done) {
+      var parentElement = document.createElement('div');
+      var element1 = document.createElement('div');
+      var element2 = document.createElement('div');
+
+      element1.setAttribute('data-module', 'domModuleElement1');
+      element2.setAttribute('data-module', 'domModuleElement2');
+
+      parentElement.appendChild(element1);
+      parentElement.appendChild(element2);
+      document.body.appendChild(parentElement);
+
+      window.Carpet.module('domModuleElement1', function (exports, settings, context) {
+        expect(context).toEqual(element1);
+        done();
+      });
+
+      expect(typeof window.Carpet.getModule('domModuleElement2')).toBe('undefined');
+
+      window.Carpet.init(element1);
+    });
+
+
   });
 
   describe('Carpet.js: Component pattern should behave correctly', function () {
