b0330b5330b9b5185006a9d11ed41bb49fd4d4fe
Mateusz <mateusz.gachowski@gmail.com>
2015-10-08 02:47:44 +0200

    Yay! Enable other browsers

--- karma.conf.js
+++ karma.conf.js
@@ -9,55 +9,55 @@ module.exports = function (config) {
       browserName: 'chrome',
       platform: 'OS X 10.11',
       version: '45.0'
+    },
+    sl_chrome_windows: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '45.0'
+    },
+
+    // Firefox
+    sl_firefox_windows: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'Windows 8',
+      version: '41.0'
+    },
+    sl_firefox_mac: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      platform: 'OS X 10.11',
+      version: '41.0'
+    },
+
+    // Safari
+    sl_safari_mac: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.11',
+      version: '8.1'
+    },
+
+    // IE
+    sl_ie_9_windows: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '9.0'
+    },
+    sl_ie_10_windows: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '10.0'
+    },
+    sl_ie_11_windows: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '11.0'
     }
-    // sl_chrome_windows: {
-    //   base: 'SauceLabs',
-    //   browserName: 'chrome',
-    //   platform: 'Windows 7',
-    //   version: '45.0'
-    // },
-
-    // // Firefox
-    // sl_firefox_windows: {
-    //   base: 'SauceLabs',
-    //   browserName: 'firefox',
-    //   platform: 'Windows 8',
-    //   version: '41.0'
-    // },
-    // sl_firefox_mac: {
-    //   base: 'SauceLabs',
-    //   browserName: 'firefox',
-    //   platform: 'OS X 10.11',
-    //   version: '41.0'
-    // },
-
-    // // Safari
-    // sl_safari_mac: {
-    //   base: 'SauceLabs',
-    //   browserName: 'safari',
-    //   platform: 'OS X 10.11',
-    //   version: '8.1'
-    // },
-
-    // // IE
-    // sl_ie_9_windows: {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 7',
-    //   version: '9.0'
-    // },
-    // sl_ie_10_windows: {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 7',
-    //   version: '10.0'
-    // },
-    // sl_ie_11_windows: {
-    //   base: 'SauceLabs',
-    //   browserName: 'internet explorer',
-    //   platform: 'Windows 7',
-    //   version: '11.0'
-    // }
   };
 
   config.set({
@@ -87,12 +87,6 @@ module.exports = function (config) {
   if (process.env.TRAVIS)
     {
         label = "TRAVIS #" + process.env.TRAVIS_BUILD_NUMBER + " (" + process.env.TRAVIS_BUILD_ID + ")";
-
-        // config.captureTimeout = 0;
-        // config.logLevel = config.LOG_DEBUG;
-        // config.transports = [
-        //     "websocket"
-        // ];
 
         config.sauceLabs.build = label;
         config.sauceLabs.startConnect = false;
